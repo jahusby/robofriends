9c592f243c5ca4f008e83c83894fcaa3c065131a
jahusby <32870250+jahusby@users.noreply.github.com>
2020-11-03 09:40:18 +0100

    with Redux

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,16 +1,15 @@
-import React from "react";
-import "./Card.css";
+import React from 'react';
 
 const Card = ({ name, email, id }) => {
   return (
-    <div className="card">
-      <img src={`https://robohash.org/${id}?size=200x200`} alt="robots" />
-      <div className="card__text">
+    <div className='tc grow bg-light-green br3 pa3 ma2 dib bw2 shadow-5'>
+      <img alt='robots' src={`https://robohash.org/${id}?200x200`} />
+      <div>
         <h2>{name}</h2>
         <p>{email}</p>
       </div>
     </div>
   );
-};
+}
 
 export default Card;
--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,20 +1,23 @@
-import React from "react";
-import Card from "./Card";
-import "./CardList.css";
+import React from 'react';
+import Card from './Card';
 
 const CardList = ({ robots }) => {
-  const cardsArray = robots.map((user, id) => {
-    return (
-      <Card
-        key={id}
-        id={robots[id].id}
-        name={robots[id].name}
-        email={robots[id].email}
-      />
-    );
-  });
-
-  return <div className="cardList">{cardsArray}</div>;
-};
+  return (
+    <div>
+      {
+        robots.map((user, i) => {
+          return (
+            <Card
+              key={i}
+              id={robots[i].id}
+              name={robots[i].name}
+              email={robots[i].email}
+              />
+          );
+        })
+      }
+    </div>
+  );
+}
 
 export default CardList;
