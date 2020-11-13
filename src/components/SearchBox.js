e2457ac78eaf7b9fba12e1c62ce81935f725b3c9
jahusby <32870250+jahusby@users.noreply.github.com>
2020-11-13 13:31:11 +0100

    commit

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,17 +1,18 @@
-import React from "react";
+import React from 'react';
 
 const SearchBox = ({ searchfield, searchChange }) => {
+  console.log('SearchBox');
   return (
-    <div className="pa2">
+    <div className='pa2'>
       <input
-        aria-label="Search Robots"
-        className="pa3 ba b--green bg-lightest-blue"
-        type="search"
-        placeholder="search robots"
+        aria-label="Search"
+        className='pa3 ba b--green bg-lightest-blue'
+        type='search'
+        placeholder='search robots'
         onChange={searchChange}
       />
     </div>
   );
-};
+}
 
 export default SearchBox;
